c58cfb32817daa139197238975be18272844e256
Giddyware <idowugidiyanuoluwa@gmail.com>
2024-12-02 08:18:20 +0100

    chore: add popular collection data

---
+++ components/popular-collection/data.ts
@@ -0,0 +1,68 @@
+export const collections = [
+  {
+    mainImages: ["/images/collection1.png", "/images/collection2.png"],
+    subImages: [
+      "/images/collection3.png",
+      "/images/collection4.png",
+      "/images/collection5.png",
+    ],
+    title: "Rogue Ape",
+    creatorImage: "/avatars/avatar-1.png",
+    creatorName: "Pixel Punk",
+  },
+  {
+    mainImages: ["/images/collection6.png", "/images/collection7.png"],
+    subImages: [
+      "/images/collection8.png",
+      "/images/collection9.png",
+      "/images/collection10.png",
+    ],
+    title: "Banana Bandit",
+    creatorImage: "/avatars/avatar-2.png",
+    creatorName: "Ether Artist",
+  },
+  {
+    mainImages: ["/images/collection11.png", "/images/collection12.png"],
+    subImages: [
+      "/images/collection13.png",
+      "/images/collection14.png",
+      "/images/collection15.png",
+    ],
+    title: "Ancient Greek Busts",
+    creatorImage: "/avatars/avatar-3.png",
+    creatorName: "Glitch Guru",
+  },
+  {
+    mainImages: ["/images/collection16.png", "/images/collection17.png"],
+    subImages: [
+      "/images/collection18.png",
+      "/images/collection19.png",
+      "/images/collection20.png",
+    ],
+    title: "Prime Ape",
+    creatorImage: "/avatars/avatar-4.png",
+    creatorName: "NFT Nerd",
+  },
+  {
+    mainImages: ["/images/collection21.png", "/images/collection22.png"],
+    subImages: [
+      "/images/collection23.png",
+      "/images/collection24.png",
+      "/images/collection25.png",
+    ],
+    title: "Sushi Sensei",
+    creatorImage: "/avatars/avatar-5.png",
+    creatorName: "Dream Weaver",
+  },
+  {
+    mainImages: ["/images/collection26.png", "/images/collection27.png"],
+    subImages: [
+      "/images/collection28.png",
+      "/images/collection29.png",
+      "/images/collection30.png",
+    ],
+    title: "Jungle Jester",
+    creatorImage: "/avatars/avatar-6.png",
+    creatorName: "Chain Genius",
+  },
+];
